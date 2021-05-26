901add6527237054434cf31fd5227e1eeb836c05
Justas383 <jgudeika@gmail.com>
2021-05-26 18:50:18 +0300

    AgeCalc.cs removed, fixed string exception

--- AgeCalculator/Program.cs
+++ AgeCalculator/Program.cs
@@ -12,66 +12,71 @@ namespace AgeCalculator
             var input = Console.ReadLine();
             input = input.Replace(" ", String.Empty);
             string[] inputArr = input.Split('/');
-            var birthDate = "";
+            DateTime birthDate = default;
+
 
             if (inputArr[0].Length == 4)
             {
-                try { birthDate = DateTime.ParseExact(inputArr[2] + "/" + inputArr[1] + "/" + inputArr[0], "dd/MM/yyyy", null).ToString("dd/MM/yyyy"); }
+                try { birthDate = DateTime.ParseExact(inputArr[2] + "/" + inputArr[1] + "/" + inputArr[0], "dd/MM/yyyy", null); }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message); //System message
                     Console.WriteLine("Please enter a valid day or month value!");
-                    
+                    return;
                 };
             }
             else
             {
-                try { birthDate = DateTime.ParseExact(input, "dd/MM/yyyy", null).ToString("dd/MM/yyyy"); }
+                try { birthDate = DateTime.ParseExact(input, "dd/MM/yyyy", null); }
                 catch (Exception ex)
                 {
                     Console.WriteLine(ex.Message); //System message
                     Console.WriteLine("Please enter a valid day or month value!");
-                  
+                    return;
                 };
                  }
 
-            DateTime inputDate = DateTime.Parse(birthDate);
+
             Console.WriteLine("Input date after conv.: " + birthDate);
 
-            string ageCalc(DateTime userDate)
-            {
+            
 
-                DateTime today = DateTime.Today;
-                int months = today.Month - userDate.Month;
-                int years = today.Year - userDate.Year;
-
-                if(today.Day < userDate.Day)
-                {
-                    months--;
-                }
-                if (months < 0)
-                {
-                    years--;
-                    months += 12;
-                }
-                int days = (today - inputDate.AddMonths((years * 12) + months)).Days;
-
-                if(years < 0) { return "Please enter a valid date, you are not even born yet!!"; }
-
-                else { return string.Format("{0} year{1}, {2} month{3} and {4} day{5}",
-                    years, (years == 1) ? "" : "s",
-                    months, (months == 1) ? "" : "s",
-                    days, (days == 1) ? "" : "s"
-                     ); }
-                
-
-                
-            }
-
-             Console.WriteLine(ageCalc(inputDate)); 
+             Console.WriteLine(ageCalc(birthDate)); 
 
           
             
+        }
+        static string ageCalc(DateTime userDate)
+        {
+
+            DateTime today = DateTime.Today;
+            int months = today.Month - userDate.Month;
+            int years = today.Year - userDate.Year;
+
+            if (today.Day < userDate.Day)
+            {
+                months--;
+            }
+            if (months < 0)
+            {
+                years--;
+                months += 12;
+            }
+            int days = (today - userDate.AddMonths((years * 12) + months)).Days;
+
+            if (years < 0) { return "Please enter a valid date, you are not even born yet!!"; }
+
+            else
+            {
+                return string.Format("{0} year{1}, {2} month{3} and {4} day{5}",
+             years, (years == 1) ? "" : "s",
+             months, (months == 1) ? "" : "s",
+             days, (days == 1) ? "" : "s"
+              );
+            }
+
+
+
         }
     }
 }
